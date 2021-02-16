e9b628419c665d53dd184db635325a3276798ea4
Jeevz10 <theghosthaunter@hotmail.com>
2021-02-16 20:13:49 +0800

    [/register] solved state issue in between forms

--- frontend/src/components/register/InputForm.js
+++ frontend/src/components/register/InputForm.js
@@ -2,38 +2,43 @@ import React, { Component } from 'react';
 import { FormGroup, InputGroup, Button } from '@blueprintjs/core';
 
 export class InputForm extends Component {
-    state = {
-        name: '',
-        email: '',
-        username: '',
-        password: '',
+    // how to save state between changing forms: https://stackoverflow.com/questions/41190571/saving-the-values-in-forms-after-leaving-the-page-in-reactjs
+    constructor(props) {
+        super(props);
+
+        if(localStorage.getItem(`formData` + this.props.activeIndex)) {
+            this.state = JSON.parse(localStorage.getItem(`formData` + this.props.activeIndex));
+        } else {
+            this.state = {
+                name: '',
+                email: '',
+                username: '',
+                password: '',
+            }
+        }
     }
 
     onNameChange = event => {
-        // console.log(event.target.value);
-        this.setState({
-            name: event.target.value
+        this.setState({ name: event.target.value }, () => {
+            localStorage.setItem(`formData` + this.props.activeIndex, JSON.stringify(this.state));
         })
     }
 
     onEmailChange = event => {
-        // console.log(event.target.value);
-        this.setState({
-            email: event.target.value
+        this.setState({ email: event.target.value }, () => {
+            localStorage.setItem(`formData` + this.props.activeIndex, JSON.stringify(this.state));
         })
     }
 
     onUsernameChange = event => {
-        // console.log(event.target.value);
-        this.setState({
-            username: event.target.value
+        this.setState({ username: event.target.value }, () => {
+            localStorage.setItem(`formData` + this.props.activeIndex, JSON.stringify(this.state));
         })
     }
 
     onPasswordChange = event => {
-        // console.log(event.target.value);
-        this.setState({
-            password: event.target.value
+        this.setState({ password: event.target.value }, () => {
+            localStorage.setItem(`formData` + this.props.activeIndex, JSON.stringify(this.state));
         })
     }
 
@@ -49,11 +54,35 @@ export class InputForm extends Component {
         return (
             <React.Fragment> 
                 <FormGroup>
-                    <InputGroup placeholder='Name' onChange={this.onNameChange} />
-                    <InputGroup placeholder='Email' onChange={this.onEmailChange} />
-                    <InputGroup placeholder='Username' onChange={this.onUsernameChange} />
-                    <InputGroup placeholder='Password' onChange={this.onPasswordChange}/>
+                    <InputGroup asyncControl='true' placeholder='Name' onChange={this.onNameChange} value={this.state.name} />
+                    <InputGroup asyncControl='true' placeholder='Email' onChange={this.onEmailChange} value={this.state.email}/>
+                    <InputGroup asyncControl='true' placeholder='Username' onChange={this.onUsernameChange} value={this.state.username}/>
+                    <InputGroup asyncControl='true' placeholder='Password' onChange={this.onPasswordChange} value={this.state.password}/>
                 </FormGroup>
+
+                {/* <form>
+                    <label>
+                        Name: 
+                        <input type='text' name='name' onChange={this.onNameChange} value={this.state.name}/>
+                    </label>
+
+                    <label>
+                        Email: 
+                        <input type='text' name='email' onChange={this.onEmailChange} value={this.state.email}/>
+                    </label>
+
+                    <label>
+                        Username:
+                        <input type='text' name='username' onChange={this.onUsernameChange} value={this.state.username}/>
+                    </label>
+
+                    <label>
+                        Password:
+                        <input type='text' name='password' onChange={this.onPasswordChange} value={this.state.password}/>
+                    </label>
+
+                </form> */}
+
                 <Button text='Save' onClick={this.onSaveButtonClicked}/>
                 <Button text='Next' onClick={this.onNextButtonClicked}/>
 
