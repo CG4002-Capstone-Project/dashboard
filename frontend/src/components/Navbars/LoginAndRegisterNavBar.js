5921001bd127dcb68aeca664e669b28db0534370
Jeevz10 <theghosthaunter@hotmail.com>
2021-02-20 16:23:11 +0800

    [/register] login navbar looks presentable

--- frontend/src/components/Navbars/LoginAndRegisterNavBar.js
+++ frontend/src/components/Navbars/LoginAndRegisterNavBar.js
@@ -2,18 +2,19 @@ import React, { Component } from 'react';
 import { NavBarDiv } from './LoginAndRegisterNavBarStyledComponents';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './LoginAndRegisterNavBar.css';
 
 export class LoginAndRegisterNavBar extends Component {
     render() {
         return (
-            <Navbar bg="dark" variant="light" fixed='top' className='navbar'>
+            <Navbar bg="dark" variant="light" fixed='top' className='navbar-dark'>
                 <Navbar.Brand href='home'> DanceEdge </Navbar.Brand>
-                <Nav variant='pills' className='justify-content-end'>
-                    <Nav.Item>
+                <Nav variant='pills' className='nav'>
+                    <Nav.Item className='item'>
                         <Nav.Link href='register'> Register </Nav.Link>
                     </Nav.Item>
-                    <Nav.Item>
+                    <Nav.Item className='item'>
                         <Nav.Link href='login'> Login </Nav.Link>
                     </Nav.Item>
                 </Nav>
