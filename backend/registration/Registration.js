f7ebfb611225f4cb7d213a666a110bfa53795194
Jeevz10 <theghosthaunter@hotmail.com>
2021-02-22 02:30:10 +0800

    (backend) [/register] one half of login done.

--- backend/registration/Registration.js
+++ backend/registration/Registration.js
@@ -1,10 +1,13 @@
 const UserModel = require('../schemas/user-schema');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
+const saltRounds = 10;
+const secret = process.env.JWT_SECRET;
 
-
-
-const UserCreate = () => {
-    const userInstance = new UserModel({ name: 'test5', email: 'jadc@jelo.com', username: 'tt5', password: 'cadfff' });
+const UserCreate = async ({ name, email, username, password }) => {
+    const hashedPassword = await hashPassword(password);
+    const userInstance = new UserModel({ name, email, username, password: hashedPassword });
     userInstance.save((err) => {
         if (err) {
             console.log(err);
@@ -12,6 +15,16 @@ const UserCreate = () => {
             console.log('saved!');
         }
     })
+    const token = generateAccessToken({ email });
+    return { accessToken: token };
+}
+
+const hashPassword = async (password) => {
+    return await bcrypt.hash(password, saltRounds);
+}
+
+const generateAccessToken = (payload) => {
+    return jwt.sign(payload, secret);
 }
 
 module.exports = UserCreate;
--- backend/registration/RegistrationController.js
+++ backend/registration/RegistrationController.js
@@ -3,7 +3,6 @@ const { body, validationResult } = require('express-validator');
 const app = express();
 
 const UserCreate = require('./Registration');
-const jwt = require('jsonwebtoken');
 
 app.post(
     '/create', [
@@ -17,6 +16,7 @@ app.post(
         }
         try {
             const response = await UserCreate(req.body);
+            return res.json(response);
 
         } catch (error) {
             return res.status(403).json({ error });
