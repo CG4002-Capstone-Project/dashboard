9d929eb79111c5414d48cd2f0483e5a2c1244146
Jeevz10 <theghosthaunter@hotmail.com>
2021-03-01 20:08:33 +0800

    (backend) coach tree schema and db request created

--- backend/registration/Registration.js
+++ backend/registration/Registration.js
@@ -2,13 +2,14 @@ const UserModel = require('../schemas/user-schema');
 const _ = require('lodash');
 
 const { hashPassword, generateAccessToken } = require('../auth/Auth');
+const CoachTreeModel = require('../schemas/coach-tree-schema');
 
 const UserCreate = async (body) => {
 
     for (let key of Object.keys(body)) {
         const hashedPassword = await hashPassword(body[key].password);
         const userInstance = new UserModel({ name: body[key].name, email: body[key].email, username: body[key].username, password: hashedPassword, role: body[key].role });
-        userInstance.save((err) => {
+        await userInstance.save((err) => {
             if (err) {
                 console.log(err);
             } else {
@@ -22,5 +23,25 @@ const UserCreate = async (body) => {
     return { accessToken: token };
 }
 
+const CoachTreeCreate = async (body) => {
+    const coachTreeInstance = new CoachTreeModel({ 
+        coach_username: body['coach'].username, 
+        traineeOne_username: body['trainee1'].username,
+        traineeTwo_username: body['trainee2'].username,
+        traineeThree_username: body['trainee3'].username 
+    });
 
-module.exports = UserCreate;
+    await coachTreeInstance.save((err) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(`${body['coach'].username} , ${body['trainee1'].username}, ${body['trainee2'].username}, ${body['trainee3'].username} saved!`);
+        }
+    })
+}
+
+
+module.exports = {
+    UserCreate,
+    CoachTreeCreate
+};
--- backend/registration/RegistrationController.js
+++ backend/registration/RegistrationController.js
@@ -3,13 +3,17 @@ const { body, validationResult } = require('express-validator');
 const { decodeAccessToken } = require('../auth/Auth');
 const app = express();
 
-const UserCreate = require('./Registration');
+const { UserCreate, CoachTreeCreate } = require('./Registration');
 
 app.post(
     '/create',
     async (req, res) => {
         try {
+            // creates user documents in users collection 
+            console.log(JSON.stringify(req.body));
             const response = await UserCreate(req.body);
+            // creates coach-trainee entity relationships in coach_trees collections 
+            await CoachTreeCreate(req.body);
             return res.json(response);
 
         } catch (error) {
@@ -20,8 +24,9 @@ app.post(
 app.post(
     '/decode',
     async (req, res) => {
-        console.log(`headers access token ${req.headers.accesstoken}` )
-        const {email, role} = decodeAccessToken(req.headers.accesstoken);
+        console.log('req ' + JSON.stringify(req.headers));
+        console.log(`headers access token ${req.headers.authorization}` )
+        const {email, role} = decodeAccessToken(req.headers.authorization);
         return res.json({email, role});
     }
 )
