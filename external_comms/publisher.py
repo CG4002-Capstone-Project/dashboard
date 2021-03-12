86394428c3ef96cbc41407911eb45a411e4de893
Jeevz10 <theghosthaunter@hotmail.com>
2021-03-12 13:44:00 +0800

    created rabbitmq script to communicate with external comms

---
+++ external_comms/publisher.py
@@ -0,0 +1,19 @@
+import pika, os, logging
+# pip install pika 
+# python publisher.py
+
+logging.basicConfig()
+
+# Parse CLODUAMQP_URL (fallback to localhost)
+url = os.environ.get('CLOUDAMQP_URL', 'amqps://yjxagmuu:9i_-oo9VNSh5w4DtBxOlB6KLLOMLWlgj@mustang.rmq.cloudamqp.com/yjxagmuu')
+params = pika.URLParameters(url)
+params.socket_timeout = 5
+
+connection = pika.BlockingConnection(params) # Connect to CloudAMQP
+channel = connection.channel() # start a channel
+channel.queue_declare(queue='pdfprocess') # Declare a queue
+# send a message
+
+channel.basic_publish(exchange='', routing_key='pdfprocess', body='User information')
+print ("[x] Message sent to consumer")
+connection.close()
