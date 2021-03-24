305a73b357e92b89aaa27a09b74cf0e81b24f26f
Jeevz10 <theghosthaunter@hotmail.com>
2021-03-24 22:42:18 +0800

    app loader

--- frontend/src/pages/App.js
+++ frontend/src/pages/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { useUser } from '../contexts/UserContext';
+import AppLoader from '../components/AppLoader/AppLoader';
 
 const AuthenticatedApp = React.lazy(() => import('./AuthenticatedApp'));
 const UnAuthenticatedApp = React.lazy(() => import('./UnAuthenticatedApp'));
 
 const App = () => {
   const { user } = useUser();
+  if (user.isFetching === true) {
+    return <AppLoader />
+  }
   // TODO add AppLoader
 
   return (
