4c04cf7e930ccc8d43998840a28bd43166182b62
Jeevz10 <theghosthaunter@hotmail.com>
2021-03-25 18:38:01 +0800

    make timestamp non unique

--- backend/schemas/user-schema.js
+++ backend/schemas/user-schema.js
@@ -7,7 +7,7 @@ const userSchema = new mongoose.Schema({
     password: { type: String, required: true, unique: false, trim: true  },
     role: { type: String, enum: ['coach', 'trainee'] },
     createdAt: { type: Date, default: Date.now() },
-    timestamp: { type: String, required: true, unique: true, trim: true, default: Date.now()},
+    timestamp: { type: String, required: true, unique: false, trim: true, default: Date.now()},
 })
 
 const UserModel = mongoose.model('user', userSchema);
