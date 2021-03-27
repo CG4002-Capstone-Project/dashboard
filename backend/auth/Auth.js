e7b93c920d4a5e431e7746d21fdd69493a8236f0
Jeevz10 <theghosthaunter@hotmail.com>
2021-03-27 14:34:18 +0800

    finalised auth checker

--- backend/auth/Auth.js
+++ backend/auth/Auth.js
@@ -1,5 +1,6 @@
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const UserModel = require('../schemas/user-schema');
 
 const saltRounds = 10;
 const secret = process.env.JWT_SECRET;
@@ -17,10 +18,42 @@ const decodeAccessToken = (token) => {
     return jwt.verify(token, secret);
 }
 
+const validateUserWithDB = (email, role) => {
+    let isUserGrantedAccess = false;
+    let role = '';
+    let email = '';
+    try {
+        await UserModel.find({ email, role }, (err, docs) => {
+            if (err) {
+                throw new Error(err);
+            }
+            // console.log('login docs ', docs);
+
+            if (docs.length = 1) {
+                isUserGrantedAccess = true;
+                name = docs[0].name;
+                role = docs[0].role;
+                email = docs[0].email;
+            }
+        })
+    } catch (error) {
+        throw new Error(error);
+    }
+
+    return isUserGrantedAccess;
+}
+
 const authChecker = (req, res, next) => {
     try {
         const accessToken = req.headers.authorization;
         const {email, role} = decodeAccessToken(accessToken);
+        const isUserAuthorized = validateUserWithDB(email, role);
+
+        if (!isUserAuthorized) {
+            return res.json({ error: 'Unauthorized' });
+        }
+
+        req.user ={email, role};
         next();
     } catch(e) {
         res.status(401);
@@ -32,4 +65,5 @@ module.exports = {
     hashPassword,
     generateAccessToken,
     decodeAccessToken,
+    authChecker
 }
