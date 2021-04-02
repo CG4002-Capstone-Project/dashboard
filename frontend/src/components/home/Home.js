b14accf88ea9d71dbed2cd9e6e83883d0f51a93d
Jeevz10 <theghosthaunter@hotmail.com>
2021-04-03 01:22:06 +0800

    solid start

--- frontend/src/components/home/Home.js
+++ frontend/src/components/home/Home.js
@@ -1,11 +1,25 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import LoginAndRegisterNavBar from '../navbars/login-register/LoginAndRegisterNavBar';
+import {
+    HomeDiv,
+    CoverVideoDiv,
+    ContentDiv
+} from './HomeStyledComponents';
 
 export class Home extends Component {
     render() {
         return (
-            <div>
-                
-            </div>
+            <React.Fragment>
+                <LoginAndRegisterNavBar />
+                <HomeDiv>
+                    <CoverVideoDiv>
+
+                    </CoverVideoDiv>
+                    <ContentDiv>
+
+                    </ContentDiv>
+                </HomeDiv>
+            </React.Fragment>
         )
     }
 }
---
+++ frontend/src/components/home/HomeStyledComponents.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+export const HomeDiv = styled.div`
+    display: flex;
+    flex-direction: column;
+    box-sizing: border-box;
+    /* border: 2px solid black; */
+
+`;
+
+export const CoverVideoDiv = styled.div`
+    box-sizing: border-box;
+    /* padding-top: 56px; */
+    display: flex;
+    height: calc(100vh - 56px);
+    /* border: 5px solid red; */
+`;
+
+export const ContentDiv = styled.div`
+    box-sizing: border-box;
+    /* border: 2px solid purple; */
+    height: 100vh;
+`;
+
